05f4b2acf42b0c279f6184cf4ee8bef34ef3af23
Danielwhyte <danwhy@gmail.com>
2015-06-01 15:46:52 +0100

    TRAVISSSSSSSSSS!

--- endpoints.js
+++ endpoints.js
@@ -3,28 +3,22 @@ var endpoints = {},
 
 endpoints['POST'] = function(req, res, callback){
     //Create (POST) a tweet
+    return callback(null, 'YAY');
 };
 
 endpoints['GET'] = function(req, res, callback){
     //return specified tweet
-    fs.readFile(__dirname + req.url, function(err, data){
-        if (err){
-            res.end(err.toString());
-        } else {
-            var ext = req.url.split('.')[1];
-            res.writeHead(200, {'Content-Type' : 'text/' + ext});
-            res.end(data);
-        }
-    });
+    return callback(null, 'BOO');
 };
 
 endpoints['DELETE'] = function(req, res, callback){
     //delete tweet
-    callback(data);
+    return callback(null, 'YAY');
 };
 
 endpoints.homepage = function(req, res, callback){
     //gets 10? most recent tweets
+    return callback(null, 'YAY');
 };
 
 module.exports = endpoints;
---
+++ test/backtest.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var endpoints = require('../endpoints.js');
+
+assert.equal(endpoints['GET']({url: 'tweet'}, {}, function(err, data){
+    return err || data;
+}), 'BOO');
+
+assert.equal(endpoints['POST']({url: 'tweet'}, {}, function(err, data){
+    return err || data;
+}), 'YAY');
+
+assert.equal(endpoints['DELETE']({url: 'tweet'}, {}, function(err, data){
+    return err || data;
+}), 'YAY');
+
+assert.equal(endpoints.homepage({url: 'tweet'}, {}, function(err, data){
+    return err || data;
+}), 'YAY');
