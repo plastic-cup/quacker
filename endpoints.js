c4487ba057eb8a3d52ca035b7787b2df7d3f4d09
Danielwhyte <danwhy@gmail.com>
2015-06-04 13:59:30 +0100

    post works

--- endpoints.js
+++ endpoints.js
@@ -1,8 +1,19 @@
 var endpoints = {},
-    fs = require('fs');
+    fs = require('fs'),
+    quax;
 
 endpoints['/main POST'] = function(req, res, callback){
-  return callback(null, 'YAY');
+    var id = new Date().getTime() + Math.floor(Math.random() * 1000),
+        quack = req.url.split('?')[1].split('=')[1],
+        time = new Date();
+
+    if (!quax){
+        quax = {};
+    }
+
+    quax[id] = {quack : quack, time : time};
+    console.log(quax);
+    return callback(null, 'YAY');
 };
 
 endpoints['/main GET'] = function(req, res, callback){
@@ -17,8 +28,20 @@ endpoints['/main DELETE'] = function(req, res, callback){
 
 endpoints.homepage = function(req, res, callback){
     fs.readFile(__dirname + '/index.html',function(err,data){
-      callback(err,data.toString());
+      callback(err, data.toString());
     });
+};
+
+endpoints.default = function(req, res, callback){
+    if (req.url.indexOf('.') > -1){
+        fs.readFile(__dirname + req.url, function(err, data){
+            if (err){
+                return callback(err);
+            } else {
+                return callback(null, data.toString());
+            }
+        });
+    }
 };
 
 module.exports = endpoints;
