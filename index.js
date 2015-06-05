dce7bd1eb3d5a17993903ba0b6c6e91102d0a35a
rjmk <rafekennedy@gmail.com>
2015-06-05 01:34:35 +0100

    changes the architecture quite a bit. Full coverage. All tests pass. Storing tweets in the file system a nightmare (the storing fights each other). To redis\!

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
 // express stuff
 var url = require('url'),
-    http = require('http'),
-    endpoints = require("./endpoints.js"),
-    routing = require("./routing.js");
+  http = require('http'),
+  endpoints = require("./endpoints.js"),
+  routing = require("./routing.js");
 
 
 function express(){
-	var middlewareStore = [];
+	  var middlewareStore = [];
 
-	function app(request, response){
-        app.routing(request, response); //populates middlewareStore;
-		app.handle(request, response);
-	}
+	  function app(request, response){
+	    	app.handle(request, response, routing(request));
+  	}
 
-    app.routing = function(request, response){
-        middlewareStore = routing(request, response);
+    app.route = function(path, fn){
+      app[path] = app[path] || {};
+      app[path].store = app[path].store || [];
+      app[path].store.push(fn);
     };
 
-
     app.use = function (fn){
-		middlewareStore.push(fn);
-	};
+	     app.route('/ GET', fn);
+    };
 
-	app.handle = function (request,response){
-		var index = 0;
-		function next(){
-            		if (middlewareStore.length >= 1){
-                		middlewareStore.shift()(request, response, next);
-            		}
-        	}
-		next();
-	};
-	return app;
+  	app.handle = function (request, response, path){
+	  	  var index = 0;
+        var that = app[path] && app[path].store ? app[path] : app.generic;
+        console.log(path);
+
+	  	  function runner(err) {
+            var lay = this.store[index];
+            if (!lay) return;
+            index++;
+            if (err && lay.length < 4) {
+              next(err);
+            } else if (err){
+              lay(err, request, response, next);
+            } else if (lay.length > 3) {
+              next();
+            } else {
+              lay(request, response, next);
+            }
+        }
+
+        next = runner.bind(that);
+        next();
+	  };
+	  return app;
 }
 
 module.exports = express;
-
-// function next(){
-//     if (middlewareStore.length >= 1){
-//         middlewareStore.shift()(request, res, next);
-//     }
-//     next();
-// }
--- server.js
+++ server.js
@@ -1,4 +1,30 @@
 var http = require("http"),
     express = require("./index.js");
+    endpoints = require("./endpoints");
+    app = express();
 
-http.createServer(express()).listen(8000);
+http.createServer(app).listen(8000);
+
+app.use(endpoints.homepage);
+app.use(endpoints['/main GET']);
+app.use(errorHandler);
+
+app.route('/main POST', endpoints['/main POST']);
+app.route('/main POST', errorHandler);
+
+app.route('/main DELETE', endpoints['/main DELETE']);
+app.route('/main DELETE', errorHandler);
+
+app.route('generic', endpoints.default);
+app.route('generic', errorHandler);
+
+
+function errorHandler(err, request, response, next){
+    console.error(err);
+    response.writeHead(500);
+    response.end(err.toString());
+}
+
+setInterval(function(){
+    fs.writeFile(__dirname + '/quax.json', JSON.stringify(quax));
+},1000);
